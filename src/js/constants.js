b2e55e54b6ab15c8b9daa27c94e75d93ef71b754
Phillip O'Donnell <phillip.odonnell@disruptor.ltd>
2025-06-20 03:15:06 +1200

    Update application version to 2025.06.20-preview-1 in constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,7 +3,7 @@
  */
 
 // Current application version
-const APP_VERSION = '2025.06.18-preview-1';
+const APP_VERSION = '2025.06.20-preview-1';
 
 // Display version with 'v' prefix for UI
 const DISPLAY_VERSION = `v${APP_VERSION}`;
