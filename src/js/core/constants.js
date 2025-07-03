bda547596a3ccf916e48a1092b4b572ce943995f
Phillip O'Donnell <phillip.odonnell@disruptor.ltd>
2025-07-03 16:54:33 +1200

    Update styles and application version
    - Commented out debug styles for dropdown elements in `styles.css` to clean up the code and improve maintainability.
    - Updated the application version in `constants.js` to '2025.07.03-beta-2' for the upcoming release.

--- src/js/core/constants.js
+++ src/js/core/constants.js
@@ -3,7 +3,7 @@
  */
 
 // Current application version
-export const APP_VERSION = '2025.06.22-beta-1'
+export const APP_VERSION = '2025.07.03-beta-2'
 
 // Display version with 'v' prefix for UI
 export const DISPLAY_VERSION = `v${APP_VERSION}`
