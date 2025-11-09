cd1ba3419039c22216eebd4772486eb2d4daca1c
Phillip O'Donnell <phillip.odonnell@gmail.com>
2025-11-09 21:30:07 +0000

    Update application version to 2025.11.07-hotfix-2 in constants.js to reflect the latest hotfix release.

--- src/js/core/constants.js
+++ src/js/core/constants.js
@@ -3,7 +3,7 @@
  */
 
 // Current application version
-export const APP_VERSION = '2025.11.07-hotfix-1'
+export const APP_VERSION = '2025.11.07-hotfix-2'
 
 // Display version with 'v' prefix for UI
 export const DISPLAY_VERSION = `v${APP_VERSION}`
