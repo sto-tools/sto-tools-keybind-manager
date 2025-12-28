6ab9eab0ae90c3b34fa06aef923e8a21da977a5f
Phillip O'Donnell <phillip.odonnell@gmail.com>
2025-12-28 22:32:50 +0000

    Update application version to 2025.12.05-hf4

--- src/js/core/constants.js
+++ src/js/core/constants.js
@@ -3,7 +3,7 @@
  */
 
 // Current application version
-export const APP_VERSION = '2025.12.05-hf3'
+export const APP_VERSION = '2025.12.05-hf4'
 
 // Display version with 'v' prefix for UI
 export const DISPLAY_VERSION = `v${APP_VERSION}`
