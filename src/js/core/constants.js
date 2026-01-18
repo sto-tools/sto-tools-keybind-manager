4c90dabed641034dafeacbce7b0e706140480c5d
Phillip O'Donnell <phillip.odonnell@gmail.com>
2026-01-18 05:09:25 +0000

    Update application version to 2026.01.18-1

--- src/js/core/constants.js
+++ src/js/core/constants.js
@@ -3,7 +3,7 @@
  */
 
 // Current application version
-export const APP_VERSION = '2026.01.18'
+export const APP_VERSION = '2026.01.18-1'
 
 // Display version with 'v' prefix for UI
 export const DISPLAY_VERSION = `v${APP_VERSION}`
